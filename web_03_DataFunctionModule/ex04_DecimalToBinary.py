d513879f7e446f6efc741e731bfa3e5cc540e048
Tatiana Kalinina <tankakalinka@gmail.com>
2022-12-06 17:21:36 +0300

    add new solution method

--- web_03_DataFunctionModule/ex04_DecimalToBinary.py
+++ web_03_DataFunctionModule/ex04_DecimalToBinary.py
@@ -1,6 +1,19 @@
 # Задача 4. Напишите программу, которая будет
 # преобразовывать десятичное число в двоичное.
 
+
+def dec_to_bin_rec(decimal):
+    if decimal == 0:
+        return ''
+    else:
+        return f'{dec_to_bin_rec(decimal // 2)}{decimal % 2}' if decimal > 0 else ''
+
+
+print(dec_to_bin_rec(46))
+
+exit()
+
+
 def decimal_to_binary(decimal):
     binary = str(decimal % 2)
     while decimal // 2 != 0:
